3e90690cee8e497cea589c593ce91672542ffffd
bhatpawan34 <150516059+bhatpawan34@users.noreply.github.com>
2023-11-11 18:26:31 +0000

    DataTypes

---
+++ Basics/DataTypes.js
@@ -0,0 +1,12 @@
+//"use strict";// treat all js code as newer version
+//alert("pawan")
+console.log("pawan")
+console.log(4 +4)
+
+let score="45pp";
+
+
+let convertScore=Number(score)
+console.log(typeof score)
+console.log(typeof(convertScore))
+console.log(convertScore)
